d9995c6634977be2ecadbb3f3b8685fbf441d85c
yashgopani41 <155447386+yashgopani41@users.noreply.github.com>
2024-02-23 15:21:51 +0530

    done

--- lib/validation/user.ts
+++ lib/validation/user.ts
@@ -1,7 +1,7 @@
 import * as z from "zod";
 
 export const UserValidation = z.object({
-  profile_photo: z.string().url(),
+  profile_photo: z.string().url().nonempty(),
   id: z.string(),
   name: z.string().min(3).max(30),
   username: z.string().min(3).max(30),
