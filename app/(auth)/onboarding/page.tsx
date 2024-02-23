b89dd98b8f100ca544472a51cab353b33d2e2835
yashgopani41 <155447386+yashgopani41@users.noreply.github.com>
2024-02-23 15:21:22 +0530

    done with onboarding

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,9 +1,31 @@
+import AccountProfile from "../../../components/forms/AccountProfile";
+import { currentUser } from "@clerk/nextjs";
+
 const Page = async () => {
+  const user = await currentUser();
+
+  const userInfo = {};
+
+  // const userData = {
+  //   id: user.id,
+  //   objectId: userInfo?._id,
+  //   username: userInfo ? userInfo?.username : user.username,
+  //   name: userInfo ? userInfo?.name : user.firstName ?? "",
+  //   bio: userInfo ? userInfo?.bio : "",
+  //   image: userInfo ? userInfo?.image : user.imageUrl,
+  // };
+
   return (
     <>
-      <div>
-        <h1>Onboarding</h1>
-      </div>
+      <main className="mx-auto flex max-w-3xl flex-col justify-start px-10 py-20">
+        <h1 className="head-text">Onboarding</h1>
+        <p className="mt-3 text-base-regular text-light-2">
+          Complete Your Profile Now to Use Threads
+        </p>
+        <section className="mt-9 bg-dark-2 p-10">
+          {/* <AccountProfile user={userData} btnTitle="Continue" /> */}
+        </section>
+      </main>
     </>
   );
 };
